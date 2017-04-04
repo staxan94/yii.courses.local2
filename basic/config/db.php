057bc2f1376d58676bdc556b59f5ec7333609c1e
root <dhcp_book@mail.ru>
2017-04-04 19:50:38 +0300

    add model Product, admin panel for product..

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=brandshop',
+    'dsn' => 'mysql:host=localhost;dbname=geekshop',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
